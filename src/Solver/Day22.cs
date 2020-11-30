558c06abbdd4bfaebb54258fa2a7687fa6908f6d
Jesper <jturesson90@gmail.com>
2020-11-30 11:23:47 +0100

    Added days

---
+++ src/Solver/Day22.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+
+namespace CSharpStarterPack.Solver
+{
+    public static class Day22
+    {
+        public static int PuzzleA()
+        {
+            return 0;
+        }
+        public static int PuzzleB()
+        {
+            return 0;
+        }
+    }
+}
